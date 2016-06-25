734ef795a89d570d4c9b0b79ff87b3040f95226a
Nelson Desai <nelson.desai@gmail.com>
2016-06-26 00:09:55 +0530

    Initial source commit

---
+++ app/app.component.ts
@@ -0,0 +1,9 @@
+import {Component} from 'angular2/core';
+import {RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
+
+@Component({
+    selector: 'my-app',
+    template: `<h1>First Project`
+})
+export class AppComponent {    
+}
